8d0619dc6a80e8719e2ce26eaa521877b51ab264
Fuerback <felipefuerback@hotmail.com>
2018-06-02 21:20:07 -0300

    template form para react form LOGIN

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../service/login.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'ca-login',
@@ -8,6 +9,10 @@ import { LoginService } from '../service/login.service';
 })
 export class LoginComponent implements OnInit {
 
+  loginForm: FormGroup;
+
+  senhaPatetrn = /^((?=.*[a-z])(?=.*[A-Z])(?=.*\d)|(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])|(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9])|(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]))([A-Za-z\d@#$%^&£*\-_+=[\]{}|\\:',?/`~"();!]|\.(?!@)){8,16}$/;
+
   constructor(private loginService: LoginService) { }
 
   login(form: any) {
@@ -15,6 +20,13 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      senha: new FormControl('', [Validators.required, Validators.pattern(this.senhaPatetrn)])
+    });
   }
+
+  get email(): any { return this.loginForm.get('email'); }
+  get senha(): any { return this.loginForm.get('senha'); }
 
 }
